bdcf997c0ba566879b12a5656aad764d5e85fd79
enriquemsalcedor <enriquem.salcedor@gmail.com>
2023-10-02 13:10:19 -0400

    security firebase 1.5

--- habilidades_proveedor.php
+++ habilidades_proveedor.php
@@ -2,6 +2,7 @@
     
     //CROSS-REEFRENCE PERMITIDOS
     header("Access-Control-Allow-Origin: *");
+    header('Access-Control-Allow-Headers: Authorization');
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
     header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
