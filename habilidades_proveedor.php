b4f75df0f8728478ff0cdc7849589ef330f3713d
enriquemsalcedor <enriquem.salcedor@gmail.com>
2023-10-04 11:15:27 -0400

    actualizacion

--- habilidades_proveedor.php
+++ habilidades_proveedor.php
@@ -1,7 +1,7 @@
 <?php
     
     //CROSS-REEFRENCE PERMITIDOS
-    header("Access-Control-Allow-Origin: *.toolkit.maxialatam.com");
+    header("Access-Control-Allow-Origin: https://toolkit.maxialatam.com");
     header('Access-Control-Allow-Credentials: false');
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
     header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
